0d092c8ad627278743a369c6090aed23ba4d88d3
nandotorres <nandotorres@gmail.com>
2013-01-12 10:00:53 -0200

    Alterando o retorno do Upload para JSON

--- app/models.py
+++ app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Video(models.Model):
     file = models.FileField(upload_to='videos/%Y/%m/%d')
     job_id = models.IntegerField(null=True, blank=True)
+    job_done = models.BooleanField(default=False)
--- app/views.py
+++ app/views.py
@@ -1,24 +1,37 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.utils import simplejson
 
 from app.models import Video
 from app.forms import VideoForm
-    
-def upload(request):
-    if request.method == 'POST':
-        form = VideoForm(request.POST, request.FILES)
 
+"""
+ upload:
+   realiza upload dos arquivos para o servidor
+   apos realizar o upload e salvar o registro, faz uma chamada a API do zencoder
+   e retorna um json com o status e id do job 
+"""
+def upload(request):
+    
+    feedback = {} #sera usado para o retorno da acao
+    
+    if request.method == 'POST':        
+        form = VideoForm(request.POST, request.FILES)
         video = Video(file = request.FILES['file'])
         video.save()
-        return HttpResponseRedirect(reverse('app.views.upload'))
+        
+        feedback["status"] = "ok"
+        feedback["job_id"] = "222"
+        
+        return HttpResponse(simplejson.dumps(feedback), mimetype="application/json")
     else:
         form = VideoForm()
 
-    return render_to_response(
-        'index.html',
-        {},
-        context_instance=RequestContext(request)
-    )
+    feedback["sttaus"] = "nok"
+    feedback["job_id"] = "0"
+    
+    return HttpResponse(simplejson.dumps(feedback), mimetype="application/json")
